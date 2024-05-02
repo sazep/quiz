8ae0b1f8a1a508db08cf0b04653cdbe65998c4e7
sazep <105110668+sazep@users.noreply.github.com>
2024-05-02 19:15:07 +0200

    Add files via upload

--- all.js
+++ all.js
@@ -2,8 +2,11 @@ let div_qw = document.querySelector('.quiz')
 let div_ans = document.querySelector('.ans')
 let ans_arr = div_ans.querySelectorAll('div')
 let allowAnswerSelection = true
+let display_menu = false
+let qws = NaN
+let selectedAnswer = true
 let questionsLog = []
-
+let resultText = NaN
 
 function randint(min,max){// генерирует от до
     let n = Math.random()
@@ -41,150 +44,143 @@ class Question {
     }
     display() {
         if (this.createdQuestions < this.totalQuestions) {
-            this.newqw();
-            this.createdQuestions++;
-            div_qw.innerHTML = this.qw;
-            shuffle(this.ans);
+            this.newqw()
+            this.createdQuestions++
+            div_qw.innerHTML = this.qw
+            shuffle(this.ans)
             for (let i = 0; i < ans_arr.length; i++) {
-                ans_arr[i].innerHTML = this.ans[i];
+                ans_arr[i].innerHTML = this.ans[i]
             }
             if (this.timer) {
-                this.timer.stop();
+                this.timer.stop()
             }
             this.timer = new Timer(21, () => {
                 if (allowAnswerSelection) {
-                    allowAnswerSelection = false;
-                    ans_arr.forEach(ans => ans.classList.add('time-up'));
+                    allowAnswerSelection = false
+                    ans_arr.forEach(ans => ans.classList.add('time-up'))
                     setTimeout(() => {
-                        ans_arr.forEach(ans => ans.classList.remove('time-up'));
-                        qws.wrong_count++;
-                        qws.display();
-                        allowAnswerSelection = true;
-                    }, 2000);
+                        ans_arr.forEach(ans => ans.classList.remove('time-up'))
+                        qws.wrong_count++
+                        qws.display()
+                        allowAnswerSelection = true
+                    }, 2000)
                 }
-            });
-            this.timer.start();
+            })
+            this.timer.start()
             for (let ans of ans_arr) {
                 ans.addEventListener('click', () => {
                     if (allowAnswerSelection) {
-                        allowAnswerSelection = false;
-                        console.log(this.timer.seconds)
-                        console.log(this.timer.maxseconds)
+                        allowAnswerSelection = false
                         let endTime = this.timer.seconds
-                        if (this.timer) {
-                            this.timer.stop();
-                        }
                         this.timeSpent = (this.timer.maxseconds - endTime)
-                        let selectedAnswer = ans.innerHTML;
+                        let selectedAnswer = ans.innerHTML
                         let questionData = {
                             question: this.qw,
                             selectedAnswer: selectedAnswer,
                             correctAnswer: this.correctAnswer,
                             allAnswers: this.ans,
                             timeSpent: this.timeSpent,
-                        };
-                        if (selectedAnswer == this.correctAnswer) {
-                            questionData.isCorrect = true;
-                            ans.classList.add('correct-answer');
-                            this.correct_count++;
-                        } else {
-                            questionData.isCorrect = false;
-                            ans.classList.add('wrong-answer');
                         }
-                        questionsLog.push(questionData);
+                        if (this.timer) {
+                            this.timer.stop()
+                        }
+                        if (selectedAnswer == this.correctAnswer) {
+                            questionData.isCorrect = true
+                            ans.classList.add('correct-answer')
+                            this.correct_count++
+                        } else {
+                            questionData.isCorrect = false
+                            ans.classList.add('wrong-answer')
+                        }
+                        questionsLog.push(questionData)
                         setTimeout(() => {
-                            ans.classList.remove('correct-answer', 'wrong-answer');
-                            this.display();
-                            allowAnswerSelection = true;
-                            console.log("Created questions:", this.createdQuestions);
-                            console.log("Total questions:", this.totalQuestions);
-                            if (this.createdQuestions >= this.totalQuestions) {
-                                console.log("   стооооооооооой ну пожалуйста :(");
-                                console.log("questionsLog:", questionsLog);
-                            }
-                        }, 2000);
+                            this.display()
+                            ans.classList.remove('correct-answer','wrong-answer')
+                            allowAnswerSelection = true
+                        }, 1000)
                     }
-                });
+                })
             }
         } else {
-            document.querySelector('.card_qw').style.display = 'none';
-            document.querySelector('.ans').style.display = 'none';
+            document.querySelector('.card_qw').style.display = 'none'
+            document.querySelector('.ans').style.display = 'none'
         
-            let totalQuestions = this.totalQuestions;
-            let errors = totalQuestions - this.correct_count;
-            let percentage = Math.round((this.correct_count / totalQuestions) * 100);
-            let resultText = document.getElementById('results');
+            let errors = this.totalQuestions - this.correct_count
+            let percentage = Math.round((this.correct_count / this.totalQuestions) * 100)
+            resultText = document.getElementById('results')
+            resultText.style.display= 'block'
+            console.log(resultText.style.display)
 
-            let totalElapsedTime = 0;
+            let totalElapsedTime = 0
             questionsLog.forEach(question => {
-                totalElapsedTime += question.timeSpent;
-            });
-            let averageTime = totalElapsedTime / this.totalQuestions;
-            
+                totalElapsedTime += question.timeSpent
+            })
+            let averageTime = totalElapsedTime / this.totalQuestions
             resultText.innerHTML = `
-            <div class='card_qw'>
-                <div>
-                    <span>Вірно:</span>
-                    <span>${this.correct_count}</span>
+            <div class='card_qw' style="display:block">
+            <h2>Ваші Результати:</h2>
+            <section>
+                <div class="result-item">
+                    <p class="result-number">${this.correct_count}</p>
+                    <p class="result-label">Вірно</p>
                 </div>
-                <div>
-                    <span>Помилок:</span>
-                    <span>${errors}</span>
+                <div class="result-item">
+                    <p class="result-number">${errors}</p>
+                    <p class="result-label">Помилок</p>
                 </div>
-                <div>
-                    <span>Питань:</span>
-                    <span>${totalQuestions}</span>
+                <div class="result-item">
+                    <p class="result-number">${averageTime.toFixed(2)}</p>
+                    <p class="result-label">Середній час відповіді (сек)</p>
                 </div>
-                <div>
-                    <span>Середній час відповіді:</span>
-                    <span>${averageTime.toFixed(2)} сек</span>
+                <div class="result-item">
+                    <p class="result-number">${percentage}%</p>
+                    <p class="result-label">Відсоток правильних відповідей</p>
                 </div>
-                <div>
-                    <span>Відсоток правильних відповідей:</span>
-                    <span>${percentage}%</span>
-                </div>
+            </section>
+                <span>Вірних відповідей: ${this.correct_count} із ${this.totalQuestions} Питаннь</span>
                 <div class="progress-bar">
-                    <div class="progress" style="width: ${percentage}%;"></div>
+                    <div class="progress" style="width: ${percentage}%"></div>
                 </div>
             </div>
-            `;
+            `
             
         
-            let questionsLogDisplay = document.createElement('div');
+            let questionsLogDisplay = document.createElement('div')
             questionsLog.forEach((questionData) => {
-                let questionContainer = document.createElement('div');
-                questionContainer.classList.add('card_qw');
+                let questionContainer = document.createElement('div')
+                questionContainer.classList.add('card_qw')
+                questionContainer.style.display='block'
         
                 let quizDisplay = document.createElement('div')
                 quizDisplay.classList.add('quiz')
                 quizDisplay.textContent = `${questionData.question} Витрачений час: ${questionData.timeSpent} секунди`
                 questionContainer.appendChild(quizDisplay)
         
-                let ansDisplay = document.createElement('div');
-                ansDisplay.classList.add('ans');
+                let ansDisplay = document.createElement('div')
+                ansDisplay.classList.add('ans')
                 questionData.allAnswers.forEach(answer => {
-                    let answerDiv = document.createElement('div');
+                    let answerDiv = document.createElement('div')
                     answerDiv.textContent = `${answer}`
                     if (answer == +questionData.selectedAnswer) {
                         if (questionData.isCorrect) {
-                            answerDiv.classList.add('selected-correct-answer');
+                            answerDiv.classList.add('selected-correct-answer')
                         } else {
-                            answerDiv.classList.add('selected-wrong-answer');
+                            answerDiv.classList.add('selected-wrong-answer')
                         }
                     }
                     if (answer == questionData.correctAnswer) {
                         if (answer != questionData.selectedAnswer) {
-                            answerDiv.classList.add('correct-answer');
+                            answerDiv.classList.add('correct-answer')
                         }
                     }
-                    ansDisplay.appendChild(answerDiv);
-                });
-                questionContainer.appendChild(ansDisplay);
-                questionsLogDisplay.appendChild(questionContainer);
-            });
+                    ansDisplay.appendChild(answerDiv)
+                })
+                questionContainer.appendChild(ansDisplay)
+                questionsLogDisplay.appendChild(questionContainer)
+            })
         
-            resultText.appendChild(questionsLogDisplay);
-            document.getElementById('results').style.display = 'block';
+            resultText.appendChild(questionsLogDisplay)
+            document.getElementById('results').style.display = 'block'
         }
         
     }
@@ -232,68 +228,82 @@ class Timer {
 
     start() {
         this.intervalId = setInterval(() => {
-            this.seconds--;
-            this.updateDisplay();
+            this.seconds--
+            this.updateDisplay()
             if (this.seconds <= 0) {
-                this.stop();
-                this.callback();
+                this.stop()
+                this.callback()
             }
-        }, 1000);
-        this.updateDisplay();
+        }, 1000)
+        this.updateDisplay()
     }
 
     stop() {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
+        clearInterval(this.intervalId)
+        this.intervalId = null
     }
 
     updateDisplay() {
-        const minutes = Math.floor(this.seconds / 60);
-        const remainingSeconds = this.seconds % 60;
-        this.timerDisplay.innerHTML = `${minutes < 10 ? '0' + minutes : minutes}:${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`;
+        const minutes = Math.floor(this.seconds / 60)
+        const remainingSeconds = this.seconds % 60
+        this.timerDisplay.innerHTML = `${minutes < 10 ? '0' + minutes : minutes}:${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`
     }
 }
 
 function startTimer(seconds, callback) {
-    let timer = new Timer(seconds, callback);
-    timer.start();
-    return timer;
+    let timer = new Timer(seconds, callback)
+    timer.start()
+    return timer
 }
-let qws = new Question(3)
-qws.display()
-for (let ans of ans_arr) {
-    ans.addEventListener('click', function() {
-        if (allowAnswerSelection) {
-            allowAnswerSelection = false;
-            let selectedAnswer = this.innerHTML; // Внутри функции используем this
-            let questionData = {
-                question: qws.qw,
-                selectedAnswer: selectedAnswer,
-                correctAnswer: qws.correctAnswer,
-                allAnswers: qws.ans,
-                //timeSpent: timeSpent
-            };
-            if (selectedAnswer == qws.correctAnswer) {
-                questionData.isCorrect = true;
-                this.classList.add('correct-answer', 'selected-correct-answer');
-                qws.correct_count++;
-            } else {
-                questionData.isCorrect = false;
-                this.classList.add('wrong-answer', 'selected-wrong-answer');
-            }
-            questionsLog.push(questionData);
-            setTimeout(() => {
-                this.classList.remove('correct-answer', 'wrong-answer');
-                qws.display();
-                allowAnswerSelection = true;
-                console.log("Created questions:", qws.createdQuestions);
-                console.log("Total questions:", qws.totalQuestions);
-                if (qws.createdQuestions >= qws.totalQuestions) {
-                    console.log("стооооооооооой ну пожалуйста :(");
-                    console.log("questionsLog:", questionsLog);
-                }
-            }, 1000);
-            timer.seconds = 21;
-        }
-    });
+
+function hideMenu() {
+    setTimeout(function() {
+        display_menu = true
+        document.querySelector('.menu').style.display = 'none'
+        document.querySelector('.card_qw').style.display = 'block'
+    }, 100)
 }
+document.getElementById('btn_logo').addEventListener('click', function() {
+    setTimeout(function() {
+        document.querySelector('.menu').style.display = 'block'
+        document.querySelector('.card_qw').style.display = 'none'
+        document.querySelector('.ans').style.display = 'flex'
+        resultText.style.display= 'none'
+        questionsLog = []
+        allowAnswerSelection = true
+        resultText = NaN
+    }, 100)
+})
+document.getElementById('btn_logo').addEventListener('click', function() {
+    setTimeout(function() {
+        display_menu = false;
+        document.querySelector('.menu').style.display = 'block';
+        document.querySelector('.card_qw').style.display = 'none';
+        document.querySelector('.ans').style.display = 'flex';
+        resultText.style.display= 'none';
+    }, 100)
+})
+
+document.getElementById('quiz3').addEventListener('click', function() {
+    hideMenu()
+    qws = new Question(3)
+    qws.display()
+})
+
+document.getElementById('quiz5').addEventListener('click', function() {
+    hideMenu()
+    qws = new Question(5)
+    qws.display()
+})
+
+document.getElementById('quiz10').addEventListener('click', function() {
+    hideMenu()
+    qws = new Question(10)
+    qws.display()
+})
+
+document.getElementById('quiz15').addEventListener('click', function() {
+    hideMenu()
+    qws = new Question(15)
+    qws.display()
+})
